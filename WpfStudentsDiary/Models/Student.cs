5daf96a05ca26ed75063be9b93d248fb54fcd552
mmakselon <magdalena.pachacz@gmail.com>
2024-09-23 22:46:47 +0200

    change AddEditView

--- WpfStudentsDiary/Models/Student.cs
+++ WpfStudentsDiary/Models/Student.cs
@@ -8,6 +8,12 @@ namespace WpfStudentsDiary.Models
 {
     public class Student
     {
+
+        public Student()
+        {
+            Group = new Group();
+        }
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- WpfStudentsDiary/ViewModels/AddEditStudentViewModel.cs
+++ WpfStudentsDiary/ViewModels/AddEditStudentViewModel.cs
@@ -1,17 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfStudentsDiary.Commands;
 using WpfStudentsDiary.Models;
 
 namespace WpfStudentsDiary.ViewModels
 {
-    public class AddEditStudentViewModel :ViewModelBase
+    public class AddEditStudentViewModel : ViewModelBase
     {
+
         public AddEditStudentViewModel(Student student = null)
         {
+            CloseCommand = new RelayCommand(Close);
+            ConfirmCommand = new RelayCommand(Confirm);
 
+            if (student == null)
+            {
+                Student = new Student();
+            }
+            else
+            {
+                Student = student;
+                IsUpdate = true;
+            }
+
+            InitGroups();
+        }
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+
+        private Student _student;
+        public Student Student
+        {
+            get { return _student; }
+            set 
+            { 
+                _student = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isUpdate;
+        public bool IsUpdate
+        {
+            get { return _isUpdate; }
+            set
+            {
+                _isUpdate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _group;
+        public ObservableCollection<Group> Groups
+        {
+            get { return _group; }
+            set
+            {
+                _group = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void Confirm(object obj)
+        {
+            if (!IsUpdate)
+                AddStudent();
+            else
+                UpdateStudent();
+
+            CloseWindow(obj as Window);
+        }
+
+        private void UpdateStudent()
+        {
+            //baza danych
+        }
+
+        private void AddStudent()
+        {
+            //baza danych
+        }
+
+        private void Close(object obj)
+        {
+            CloseWindow(obj as Window);
+        }
+
+        private void CloseWindow(Window window)
+        {
+            window.Close();
+        }     
+
+        private void InitGroups()
+        {
+            var groups = new ObservableCollection<Group>
+            {
+            new Group { Id=0, Name="-- brak --"},
+            new Group { Id=1, Name="1A"},
+            new Group { Id=2, Name="2A"},
+            };
+
+            Groups = new ObservableCollection<Group>(groups);
+
+            SelectedGroupId = Student.Group.Id;
         }
     }
 }
