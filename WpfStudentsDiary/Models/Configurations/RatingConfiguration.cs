63177f9eb0080a9fd462ea47d4f29736d47d8c4a
mmakselon <magdalena.pachacz@gmail.com>
2024-09-24 21:48:19 +0200

    new Configurations

---
+++ WpfStudentsDiary/Models/Configurations/RatingConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using WpfStudentsDiary.Models.Domains;
+using System.Data.Entity.ModelConfiguration;
+
+namespace WpfStudentsDiary.Models.Configurations
+{
+    public class RatingConfiguration : EntityTypeConfiguration<Rating>
+    {
+        public RatingConfiguration()
+        {
+            ToTable("dbo.Ratings");
+
+            HasKey(x => x.Id);
+        }
+    }
+}
