429e2a241663deda7a1e9eec443ff45aedd5b6ad
mmakselon <magdalena.pachacz@gmail.com>
2024-09-27 12:59:46 +0200

    change migrations

--- WpfStudentsDiary/Models/Configurations/StudentConfiguration.cs
+++ WpfStudentsDiary/Models/Configurations/StudentConfiguration.cs
@@ -15,15 +15,6 @@ namespace WpfStudentsDiary.Models.Configurations
             ToTable("dbo.Students");
 
             HasKey(x => x.Id);
-
-            Property(x => x.FirstName)
-                .HasMaxLength(100)
-                .IsRequired();
-
-            Property(x => x.LastName)
-                .HasMaxLength(100)
-                .IsRequired();
-
         }
     }
 }
